325fa739c5f76b7e311b48b22a84d557b69f9a2a
ZeuS <69910617+ChitsaroopSingh@users.noreply.github.com>
2026-06-23 21:36:15 +0530

    added color as a parameter instead of zipping it with the bodies

--- physics/body.py
+++ physics/body.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 class Body:
-    def __init__(self, mass, position, velocity):
+    def __init__(self, mass, position, velocity, color):
         self.mass = mass
         self.position = np.array(position, dtype = float)
         self.velocity = np.array(velocity, dtype = float)
         self.radius = max(15, int(self.mass ** 0.3))
+        self.color = color
 
         self.trail = []
         
