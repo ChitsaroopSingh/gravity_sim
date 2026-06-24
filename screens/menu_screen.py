2e205c99ba2c020e823ff26094dd469e4ceb8353
ZeuS <69910617+ChitsaroopSingh@users.noreply.github.com>
2026-06-25 00:54:22 +0530

    created skeleton for screen

---
+++ screens/menu_screen.py
@@ -0,0 +1,16 @@
+class MenuScreen:
+    def __init__(self):
+        
+        self.red_mass= "100"
+        self.blue= "100"
+        self.green_mass= "100"
+
+    def handle_events(self,events):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def draw(self, screen):
+        pass
+    
