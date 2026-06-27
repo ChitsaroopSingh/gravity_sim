e79dd96a0fee8fc4c8d3de8cc4533f44bf8571d4
ZeuS <69910617+ChitsaroopSingh@users.noreply.github.com>
2026-06-28 00:36:47 +0530

    setup screen- currently not in use

---
+++ screens/setup_screen.py
@@ -0,0 +1,21 @@
+import pygame
+
+class SetupScreen:
+    def __init__(self):
+
+        self.bodies = []
+
+        self.pending_body = None
+
+        self.current_mass = 100
+
+        self.dragging = False
+
+    def handle_events(self, events):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        pass
