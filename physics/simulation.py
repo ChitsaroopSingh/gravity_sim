e11a26eadaba3a5188268e8c3822c847c9cb770b
ZeuS <69910617+ChitsaroopSingh@users.noreply.github.com>
2026-06-28 01:29:36 +0530

    using leapfrog integration

--- physics/simulation.py
+++ physics/simulation.py
@@ -1,20 +1,30 @@
 from physics.gravity import compute_acceleration
 import numpy as np
 
+#using leapfrog velocity verlet integration instead of euler integration to conserve more energy
 def update_bodies(bodies, dt):
 
     accelerations = []
 
     for body in bodies:
-        acc = compute_acceleration(body, bodies)
-        accelerations.append(np.array(acc))
+        accelerations.append(np.array(compute_acceleration(body,bodies)))
+
     
     for body, acc in zip(bodies, accelerations):
-        body.velocity += acc * dt
+        body.velocity += 0.5 * acc * dt
+
+    for body in bodies:
         body.position += body.velocity * dt
 
-        body.trail.append((body.position[0],body.position[1]))
+        body.trail.append(tuple(body.position))
 
         if len(body.trail) > 500 :
             body.trail.pop(0)
-            
+
+    new_accelerations = []
+
+    for body in bodies:
+        new_accelerations.append(np.array(compute_acceleration(body,bodies)))
+
+    for body,acc in zip(bodies, new_accelerations):
+        body.velocity += 0.5 * acc * dt
