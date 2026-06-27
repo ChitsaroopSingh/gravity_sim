71127b6f2dfe63a269b5048f2be979d3afe85c95
ZeuS <69910617+ChitsaroopSingh@users.noreply.github.com>
2026-06-28 00:41:53 +0530

    simulation screen - currently not in use

---
+++ screens/simulation_screen.py
@@ -0,0 +1,14 @@
+class SimulationScreen:
+
+    def __init__(self, bodies):
+
+        self.bodies = bodies
+
+    def handle_events(self, events):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def draw(self, screen):
+        pass
