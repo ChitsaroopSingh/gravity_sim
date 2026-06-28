676cbef9a2a58eef8abd12d78d3e62447257f8a9
ZeuS <69910617+ChitsaroopSingh@users.noreply.github.com>
2026-06-28 16:42:57 +0530

    added text for orbits

--- ui/panel.py
+++ ui/panel.py
@@ -27,11 +27,15 @@ class Panel:
 
         screen.blit(title, (panel_rect.left + 20, 20))
 
+        y = 70
+
+        heading = self.text_font.render("Simulation", True, (255,255,120))
+        
         # Divider
         pygame.draw.line(screen,(80, 80, 80),(panel_rect.left + 15, 60),(panel_rect.right - 15, 60),2)
 
-        # Stats
-        y = 80
+        screen.blit(heading,(panel_rect.left+20, y))
+        y+=35
 
         for key, value in stats.items():
 
@@ -41,6 +45,41 @@ class Panel:
                 (220, 220, 220)
             )
 
-            screen.blit(text, (panel_rect.left + 20, y))
+            screen.blit(text, (panel_rect.left + 30, y))
 
+            y += 28
+
+        y+=15
+
+        pygame.draw.line(screen,(70,70,70),(panel_rect.left+15,y),(panel_rect.right-15,y),2)
+
+        y+=20
+
+        #presets
+
+        heading=self.text_font.render("Presets",True,(255,255,120))
+
+        screen.blit(heading,(panel_rect.left+20,y))
+        y+=40
+
+        presets = [
+        "Random System",
+        "Sun - Earth",
+        "Earth - Moon",
+        "Binary Stars",
+        "Figure-8 Orbit",
+        "Infinity Orbit",
+        "Solar System"
+        ]
+
+        for preset in presets:
+            txt = self.text_font.render(preset,True,(170,220,255))
+
+            screen.blit(txt, (panel_rect.left+30,y))
             y += 30
+
+        y+=15
+
+        pygame.draw.line(screen,(70,70,70),(panel_rect.left+15,y),(panel_rect.right-15,y),2)
+
+        y += 20
