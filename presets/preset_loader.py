198f5bc893344558b8fdd08a2eee1233a7857de0
ZeuS <69910617+ChitsaroopSingh@users.noreply.github.com>
2026-07-01 01:10:47 +0530

    feat: calls all presets

--- presets/preset_loader.py
+++ presets/preset_loader.py
@@ -1,30 +1,30 @@
-# from presets.random import random_system
-from presets.solar import sun_earth
-# from presets.earthmoon import earth_moon
-# from presets.binary import binary_stars
-# from presets.figure8 import figure8
-# from presets.infinity import infinity_orbit
+from presets.random import random_system
+from presets.solar import solar
+from presets.earthmoon import earth_moon
+from presets.binary import binary_stars
+from presets.figure8 import figure8
+from presets.infinity import infinity_orbit
 from physics.body import Body
 
 
 def load_preset(name):
     print(name)
-    # if name == "Random":
-    #     return random_system()
+    if name == "Random":
+        return random_system()
 
-    if name == "Sun-Earth":
-        return sun_earth()
+    elif name == "Solar-System":
+        return solar()
 
-    # elif name == "Earth-Moon":
-    #     return earth_moon()
+    elif name == "Earth-Moon":
+         return earth_moon()
 
-    # elif name == "Binary Stars":
-    #     return binary_stars()
+    elif name == "Binary Stars":
+        return binary_stars()
 
-    # elif name == "Figure-8":
-    #     return figure8()
+    elif name == "Figure-8":
+        return figure8()
 
-    # elif name == "Infinity":
-    #     return infinity_orbit()
+    elif name == "Infinity":
+        return infinity_orbit()
 
-    #return []
+    return []
